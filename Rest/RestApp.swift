041091be8d0fc5118540800bd938e99002685f3a
RAKHYUN <kyle2598@gmail.com>
2023-11-30 11:43:43 -0600

    Initial Commit

---
+++ Rest/RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestApp.swift
+//  Rest
+//
+//  Created by Rakhyun Kim on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
